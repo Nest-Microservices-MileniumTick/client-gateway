80310583b35a6d05ce24846f499ceb0574a52ab0
Josue Chavarria M <jchavarria@jchavarria.>
2025-03-24 02:41:46 -0600

    feat: env vars for funtional ms

---
+++ src/config/env.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import * as v from 'valibot';
+
+const envSchema = v.object({
+    PORT: v.pipe(v.string(), v.transform((input) => Number(input))),
+    PRODUCTS_MICROSERVICE_HOST: v.pipe(v.string()),
+    PRODUCTS_MICROSERVICE_PORT: v.pipe(v.string(), v.transform((input) => Number(input))),
+})
+
+export const ENV = v.parse(envSchema, process.env)
